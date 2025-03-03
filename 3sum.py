b7979b69d79a536b0eff1c10782c4bf1970b4e9a
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-03 13:32:35 +0330

    🎉 adding a new project

---
+++ 3sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def threeSum(self, nums):
+        final_list = []
+        range_ = nums
+        for i in range(len(range_)):
+            range_.remove(i)
+            for k in range(len(range_)):
+                range_.remove(k)
+                for j in range(len(range_)):
+                    if abs(nums[i]) == abs((nums[k]) + (nums[j])):
+                        new_list = [num[i], num[k], num[j]]
+                        final_list.append(new_list)
+        return final_list
+
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        

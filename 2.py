4899cbaabf48fe1dc35f5c3b0d378e227e1649ce
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-07-04 04:22:14 +0330

    🐛 code fixed

--- 2.py
+++ 2.py
@@ -1,29 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+def linked_list_to_string(head):
+    current = head
+    result = ""
+    while current:
+        result = str(current.val) + result
+        current = current.next
+    return result
+
+def number_to_linked_list(final):
+    if final == 0:
+        return ListNode(0)
+    dummy = ListNode()
+    current = dummy
+    for digit in str(final)[::-1]:
+        current.next = ListNode(int(digit))
+        current = current.next
+    return dummy.next
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        list_1 = []
-        list_2 = []
-        final_list = []
-        num1 = int("".join(map(str, l1)))
-        num2 = int("".join(map(str, l2)))  
-        
-        for i in range(len(num1)-1 , -1, -1):
-            list_1.append(num1[i])
-        for i in range(len(num2)-1 , -1, -1):
-            list_2.append(num2[i])
-
-        sum_list = "".join(list_1) + "".join(list_2)
-
-        sum_list.split()
-
-        for i in range(len(sum_list)-1 , -1, -1):
-            final_list.append(sum_list[i])
-
-        return final_list
+        s1 = linked_list_to_string(l1)
+        s2 = linked_list_to_string(l2)
+        final = int(s1) + int(s2)
+        return number_to_linked_list(final)
+    
 
         """
         :type l1: Optional[ListNode]

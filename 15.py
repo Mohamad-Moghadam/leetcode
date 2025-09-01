4c4d8161ef74d5030041dedfa7d24423d73799a5
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-01 20:21:06 +0330

    🐛 15 fixed

--- 15.py
+++ 15.py
@@ -1,16 +1,28 @@
 class Solution(object):
     def threeSum(self, nums):
-        final_list = []
-        range_ = nums
-        for i in range(len(range_)):
-            range_.remove(i)
-            for k in range(len(range_)):
-                range_.remove(k)
-                for j in range(len(range_)):
-                    if abs(nums[i]) == abs((nums[k]) + (nums[j])):
-                        new_list = [nums[i], nums[k], nums[j]]
-                        final_list.append(new_list)
-        return final_list
+        final = set()
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                threesum = nums[i] + nums[left] + nums[right]
+                if threesum < 0:
+                    left += 1
+                elif threesum > 0:
+                    right -= 1
+                else:
+                    final.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+
+        return list(i for i in final)
 
         """
         :type nums: List[int]

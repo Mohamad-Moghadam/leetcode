43d026638309ff2afe6d07080429da6d1baf138e
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-01 19:20:12 +0330

    🐛 3 fixed

--- 3.py
+++ 3.py
@@ -1,22 +1,28 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        counter = 0
-        longest = [0]
-        seen = []
-        for i in range(len(s)):
-            if s[i] not in seen:
-                counter += 1
-                seen.extend(s[i])
-            else:
-                seen = []
-                seen.extend(s[i])
-                longest.append(counter)
-                counter = 1
-        return longest
+        if s == "":
+            return 0
+        longest = []
+        counter = 1
+        the_list = list(s)
+        for i in range(len(the_list)):
+            sub = [the_list[i]]
+            longest.append(counter)
+            counter = 1
+            for j in range(i+1, len(the_list)):
+                if the_list[j]:
+                    if the_list[j] not in sub:
+                        counter += 1
+                        sub.append(the_list[j])
+                    else:
+                        del sub[:]
+                        break
+
+        return max(longest)
+                
+
+
         """
         :type s: str
         :rtype: int
         """
-
-a = Solution()
-print(a.lengthOfLongestSubstring("c"))

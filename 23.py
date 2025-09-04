3e6d16f5603f4d6f98cd9a8eaaf446cb0901433c
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-05 01:03:25 +0330

    🎉 23 added

--- 23.py
+++ 23.py
@@ -3,14 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
+
 class Solution(object):
+    class ListNode(object):
+        def __init__(self, val=0, next=None):
+            self.val = val
+            self.next = next
+
     def mergeKLists(self, lists):
-        unordered_list = []
-        for i in lists:
-            for j in i:
-                unordered_list.append(j)
-        ordered_list = unordered_list.sort()
-        return ordered_list
+        heap = []
+        counter = 0
+
+        for node in lists:
+            if node:
+                heapq.heappush(heap, (node.val, counter, node))
+                counter += 1
+
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            val, _, node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, counter, node.next))
+                counter += 1
+
+        return dummy.next
+        
 
         """
         :type lists: List[Optional[ListNode]]
